ccc5a8d72e2bcbe8ba13ef1ad78a42b48bb0f681
phamtronghieu2002 <109363404+phamtronghieu2002@users.noreply.github.com>
2023-10-10 10:20:00 +0700

    Delete src/components/Bai02 directory
    ok

--- src/components/Bai02/Bai02.js
+++
@@ -1,9 +0,0 @@
-// import { View, Text } from "react-native";
-// import Bai1 from "../Bai01/Bai1";
-// function Bai2() {
-//   return (
-//     <Bai1 textBai2={true}/>
-//   );
-// }
-
-// export default Bai2;
